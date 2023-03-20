c2f9991a8c126b4febd761afd43621c83f1a5469
Stepashka81 <ptashets81@gmail.com>
2023-03-21 00:05:51 +0200

    add some compponents on my project

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Header=()=>{
+    return (
+        <header className='header'>
+        <img src="https://picsum.photos/id/0/367/267"></img>
+      </header>
+    )
+}
+export default Header
---
+++ src/components/Profile.js
@@ -0,0 +1,32 @@
+import React from "react";
+const Profile=()=>{
+    return (
+      <div className='content'>
+        <div>
+          <img src='https://picsum.photos/id/11/1000/250' ></img>
+        </div>
+        <div>
+          <img src="https://picsum.photos/id/64/50/50"></img>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+          </p>
+        </div>
+
+        <div>
+          my posts
+          <div>
+            new post
+          </div>
+          <div>
+            post1
+          </div>
+          <div>
+            post2
+          </div>
+        </div>
+
+
+      </div>
+    )
+}
+export default Profile
